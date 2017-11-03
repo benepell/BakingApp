9a11df324197fa73c5922a86261e63536cd0d166
benedetto pellerito <benedettopellerito@gmail.com>
2017-11-03 16:23:06 +0100

    SharedPref clean
    clean

--- app/src/main/java/it/instantapps/bakingapp/widget/RecipeAppWidget.java
+++ app/src/main/java/it/instantapps/bakingapp/widget/RecipeAppWidget.java
@@ -10,7 +10,6 @@ import android.graphics.Bitmap;
 import android.view.View;
 import android.widget.RemoteViews;
 
-import java.util.Locale;
 import java.util.Objects;
 
 import it.instantapps.bakingapp.R;
